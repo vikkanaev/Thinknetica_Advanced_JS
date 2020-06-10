abeb6cac920355c0bdaf6ce47fe78b9733b894bb
Viktor S Kanaev <vikkanaev@gmail.com>
2020-06-10 11:00:30 +0300

    Task-1

---
+++ Lesson_6/Task-1/ship-factory.js
@@ -0,0 +1,22 @@
+'use strict';
+const {Ship} = require('./ship.js');
+const {Position} = require('./position.js');
+
+/**
+ * Фабрика по созданию новых кораблей
+ */
+function ShipFactory() {
+  this._ships = [];
+  this.ships = () => this._ships;
+  this.shipNames = () => this._ships.map((s) => s.name);
+  this.makeNewShip = (name, model, position) => {
+    if (this.shipNames().includes(name)) throw new Error('This ship name already exists.');
+    if (!(position instanceof Position)) throw new Error('Wrong input. Position object required.');
+
+    const newShip = new Ship(name, model, position);
+    this._ships.push(newShip);
+    return newShip;
+  };
+}
+
+module.exports = {ShipFactory};
